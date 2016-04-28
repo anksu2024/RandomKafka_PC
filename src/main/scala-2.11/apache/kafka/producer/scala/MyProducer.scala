7cba08c5b72ad365d5e99ba1777a24165b5c8e8b
Ankit Sarraf <sarrafankit@hotmail.com>
2016-04-28 16:26:26 -0700

    Added Kafka Producer (Scala)

--- src/main/scala-2.11/apache/kafka/producer/scala/MyProducer.scala
+++ src/main/scala-2.11/apache/kafka/producer/scala/MyProducer.scala
@@ -1,10 +1,48 @@
 package apache.kafka.producer.scala
 
+import java.util.{Properties, Random}
+
+import kafka.javaapi.producer.Producer
+import kafka.producer.{KeyedMessage, ProducerConfig}
+
 /**
   * Project Name: KafkaProducer (Scala)
-  * Dated: April 04, 2016
+  * Dated: April 28, 2016
   */
 
-class MyProducer {
+class MyProducer private (val kafkaBroker: String, val topic: String,
+                 val events: Int, val intraEventDuration: Long,
+                 var props: Properties, var config: ProducerConfig,
+                 var kafkaProducer: Producer[String, String]) {
 
+  def this(kafkaProducer: String, topic: String, events: Int, intraEventDuration: Long) =
+    this(kafkaBroker,topic,events,intraEventDuration,null,null,null)
+
+  // Setting up properties to be used to communicate to kafka
+  props = new Properties
+  props.put("metadata.broker.list", kafkaBroker)
+  props.put("serializer.class", "kafka.serializer.StringEncoder")
+  props.put("partitioner.class", "kafka.producer.DefaultPartitioner")
+  props.put("request.required.acks", "1")
+
+  config = new ProducerConfig(props)
+  kafkaProducer = new Producer(config)
+
+  def processAndPush(): Unit = {
+    var myData: String = ""
+    val random = new Random
+
+    var message: KeyedMessage[String, String] = null
+
+    for (i <- 0 to this.events) {
+      myData = "<" + random.nextFloat + ">"
+      message = new KeyedMessage[String, String](this.topic, myData)
+      println("Data Produced -> " + message)
+      this.kafkaProducer.send(message)
+
+      Thread.sleep(this.intraEventDuration)
+    }
+
+    kafkaProducer.close
+  }
 }
--- src/main/scala-2.11/apache/kafka/producer/scala/RandomKafkaProducer.scala
+++ src/main/scala-2.11/apache/kafka/producer/scala/RandomKafkaProducer.scala
@@ -2,9 +2,29 @@ package apache.kafka.producer.scala
 
 /**
   * Project Name: KafkaProducer (Scala)
-  * Dated: April 04, 2016
+  * Dated: April 28, 2016
   */
 
-class RandomKafkaProducer {
+object RandomKafkaProducer {
+  def main(args: Array[String]) {
+    if(args.length != 4) {
+      println("Usage: java -cp <kafka-broker:port>" +
+        "<topic> <# of events> <duration between each event>")
+      return
+    }
 
+    val kafkaBroker = args(0)
+    val topic = args(1)
+    val events = args(2).toInt
+    val intraEventDuration = args(3).toLong
+
+    println("Hello from Random Kafka Producer!!")
+    val kafka_producer = new MyProducer(kafkaBroker, topic, events, intraEventDuration)
+
+    println("Sending Producer Data!!")
+
+    kafka_producer.processAndPush()
+
+    println("Data Sent to the Broker.. Pull Data from Consumer!!")
+  }
 }
